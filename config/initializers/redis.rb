a65a744ae2f8f9e0d28473d6c793649a809d29d5
HM Tanbir <tanbir2025@gmail.com>
2026-06-09 11:48:10 +0600

    Update docker compose file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
-redis_user_url = ENV.fetch("REDIS_URL") {
-  "redis://#{ENV.fetch("REDIS_HOST", "localhost")}:#{ENV.fetch("REDIS_PORT", 6379)}/#{ENV.fetch("REDIS_USER_DB", 0)}"
+redis_url = ENV.fetch("REDIS_URL") {
+  password = ENV["REDIS_PASSWORD"].present? ? ":#{ENV["REDIS_PASSWORD"]}@" : ""
+  "redis://#{password}#{ENV.fetch("REDIS_HOST", "localhost")}:#{ENV.fetch("REDIS_PORT", 6379)}/#{ENV.fetch("REDIS_DB", 0)}"
 }
 
-$redis_user_db = Redis.new(url: redis_user_url)
+$redis = Redis.new(url: redis_url)
